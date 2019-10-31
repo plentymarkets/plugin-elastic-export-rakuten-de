f86c1cd7aef41ab23e4fcaab33526fe44f6e5596
Joshua Schaak <joshua.schaak@plentymarkets.com>
2019-10-31 08:17:01 +0100

    ADUJUSTMENT removed unnecessary stuff and made style adjustments

--- src/Helper/PriceHelper.php
+++ src/Helper/PriceHelper.php
@@ -7,7 +7,6 @@ use Plenty\Modules\Item\SalesPrice\Contracts\SalesPriceRepositoryContract;
 use Plenty\Modules\Item\SalesPrice\Models\SalesPrice;
 use Plenty\Modules\Helper\Models\KeyValue;
 use Plenty\Modules\Order\Currency\Contracts\CurrencyConversionSettingsRepositoryContract;
-use Plenty\Modules\Order\Currency\Contracts\CurrencyRepositoryContract;
 
 /**
  * Class ItemUpdatePriceHelper
@@ -18,13 +17,6 @@ class PriceHelper
 {
     const TRANSFER_RRP_YES = 1;
     const TRANSFER_OFFER_PRICE_YES = 1;
-    const NET_PRICE = 'netPrice';
-    const GROSS_PRICE = 'grossPrice';
-
-    /**
-     * @var CurrencyRepositoryContract
-     */
-    private $currencyRepositoryContract;
 
     /**
      * @var SalesPriceRepositoryContract
@@ -47,11 +39,6 @@ class PriceHelper
     private $priceDetectionService = null;
 
     /**
-     * @var string
-     */
-    private $currency = null;
-
-    /**
      * @var int
      */
     private $countryId = null;
@@ -64,16 +51,13 @@ class PriceHelper
     /**
      * ItemUpdatePriceHelper constructor.
      *
-     * @param CurrencyRepositoryContract $currencyRepositoryContract
      * @param SalesPriceRepositoryContract $salesPriceRepository
      * @param CurrencyConversionSettingsRepositoryContract $currencyConversionSettingsRepositoryContract
      */
     public function __construct(
-        CurrencyRepositoryContract $currencyRepositoryContract,
         SalesPriceRepositoryContract $salesPriceRepository,
         CurrencyConversionSettingsRepositoryContract $currencyConversionSettingsRepositoryContract)
     {
-        $this->currencyRepositoryContract = $currencyRepositoryContract;
         $this->salesPriceRepository = $salesPriceRepository;
 
         $this->currencyConversionList = $currencyConversionSettingsRepositoryContract->getCurrencyConversionList();
@@ -87,7 +71,6 @@ class PriceHelper
     private function preload(KeyValue $settings)
     {
         $this->countryId = $settings->get('destination');
-        $this->currency = $this->currencyRepositoryContract->getCountryCurrency($this->countryId)->currency;
 
         $this->priceDetectionService = pluginApp(PriceDetectionService::class);
         $this->priceDetectionService->preload($settings);
@@ -124,26 +107,20 @@ class PriceHelper
         $variationPriceUpdatedTimestamp = $retailPrice['updatedAt'];
 
         // Get the recommended retail price
-        if($settings->get('transferRrp') == self::TRANSFER_RRP_YES)
-        {
+        if ($settings->get('transferRrp') == self::TRANSFER_RRP_YES) {
             $recommendedRetailPrice = $this->priceDetectionService->getPriceByPreloadList($preloadedPrices, PriceDetectionService::RRP);
             $variationRrp = $this->getPriceByRetailPriceSettings($recommendedRetailPrice, $settings);
             $rrpUpdatedTimestamp = $recommendedRetailPrice['updatedAt'];
-        }
-        else
-        {
+        } else {
             $variationRrp = 0.00;
         }
 
         // Get the special price
-        if($settings->get('transferOfferPrice') == self::TRANSFER_OFFER_PRICE_YES)
-        {
+        if ($settings->get('transferOfferPrice') == self::TRANSFER_OFFER_PRICE_YES) {
             $specialPrice = $this->priceDetectionService->getPriceByPreloadList($preloadedPrices, PriceDetectionService::SPECIAL_PRICE);
             $variationSpecialPrice = $this->getPriceByRetailPriceSettings($specialPrice, $settings);
             $specialPriceUpdatedTimestamp = isset($specialPrice['updatedAt']) ? $specialPrice['updatedAt'] : 0;
-        }
-        else
-        {
+        } else {
             $variationSpecialPrice = 0.00;
         }
 
@@ -153,11 +130,9 @@ class PriceHelper
         $referenceReducedPrice = '';
         $reducedPriceUpdatedTimestamp = '';
 
-        if ($price != '' || $price != 0.00)
-        {
+        if ($price != '' || $price != 0.00) {
             //if recommended retail price is set and higher than retail price...
-            if ($variationRrp > 0 && $variationRrp > $variationPrice)
-            {
+            if ($variationRrp > 0 && $variationRrp > $variationPrice) {
                 //set recommended retail price as selling price
                 $price = $variationRrp;
                 //set retail price as reduced price
@@ -170,15 +145,12 @@ class PriceHelper
             }
 
             // if special offer price is set and lower than retail price and recommended retail price is already set as reference...
-            if ($variationSpecialPrice > 0 && $variationPrice > $variationSpecialPrice && $referenceReducedPrice == 'UVP')
-            {
+            if ($variationSpecialPrice > 0 && $variationPrice > $variationSpecialPrice && $referenceReducedPrice == 'UVP') {
                 //set special offer price as reduced price
                 $reducedPrice = $variationSpecialPrice;
                 $reducedPriceUpdatedTimestamp = $specialPriceUpdatedTimestamp;
-            }
             //if recommended retail price is not set as reference then ...
-            elseif ($variationSpecialPrice > 0 && $variationPrice > $variationSpecialPrice)
-            {
+            } elseif ($variationSpecialPrice > 0 && $variationPrice > $variationSpecialPrice) {
                 //set special offer price as reduced price and...
                 $reducedPrice = $variationSpecialPrice;
                 $reducedPriceUpdatedTimestamp = $specialPriceUpdatedTimestamp;
@@ -229,20 +201,18 @@ class PriceHelper
             {
                 $price = $price * $this->currencyConversionList['list']['EUR']['exchange_ratio'];
                 return $price;
-            }
-            elseif(array_key_exists($priceData['salesPriceId'], $this->salesPriceCurrencyList) &&
-                $this->salesPriceCurrencyList[$priceData['salesPriceId']] === false)
+            } elseif (array_key_exists($priceData['salesPriceId'], $this->salesPriceCurrencyList) &&
+                      $this->salesPriceCurrencyList[$priceData['salesPriceId']] === false)
             {
                 return $price;
             }
 
             $salesPriceData = $this->salesPriceRepository->findById($priceData['salesPriceId']);
 
-            if($salesPriceData instanceof SalesPrice) {
+            if ($salesPriceData instanceof SalesPrice) {
                 $salePriceCurrencyData = $salesPriceData->currencies->whereIn('currency', [$this->currencyConversionList['default'], "-1"]);
 
-                if (count($salePriceCurrencyData))
-                {
+                if (count($salePriceCurrencyData)) {
                     $this->salesPriceCurrencyList[$priceData['salesPriceId']] = true;
 
                     $price = $price * $this->currencyConversionList['list']['EUR']['exchange_ratio'];
